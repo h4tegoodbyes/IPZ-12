1b208dd4d0bed4e2f1964d6f288ce428236e4758
MihailKruts <99175564+MihailKruts@users.noreply.github.com>
2022-04-15 12:33:51 +0300

    Add files via upload

---
+++ Круць Михайло Ярославович/Lab_1_ASD/Node.cs
@@ -0,0 +1,243 @@
+﻿using System.Diagnostics;
+
+namespace Lab_1_ASD
+{
+    internal class Node
+    {
+        public int CountNode;
+        public int elem;
+        public Node next_ind;
+        Node head;
+
+
+        public Node() 
+        {
+            head = null;
+            CountNode = 0;
+        }  
+        public Node(int data)
+        {
+            elem = data;
+            next_ind = null;
+        }
+        
+
+
+        public void AddFront(int data)
+        {
+            Node node = new Node(data);
+            node.next_ind = head;
+            head = node;
+            CountNode++;
+        }
+
+        public void PrintList()
+        {
+            Node temp = head;
+            Console.Write("[ ");
+            while (temp != null)
+            {
+                Console.Write(temp.elem + " ");
+                temp = temp.next_ind;
+            }
+            Console.WriteLine("]");
+        }
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = new Node(data);
+                return;
+            }
+
+            newNode.next_ind = null;
+            Node last = head;
+            while (last.next_ind != null)
+                last = last.next_ind;
+            last.next_ind = newNode;
+        }
+        public void Linear(int data)
+        {
+            var time = new Stopwatch();
+            time.Start();
+            Node temp = head;
+            bool found = false;
+            while (temp != null && found != true)
+            {
+                if (temp.elem == data)
+                {
+                    Console.WriteLine("Node found: " + temp.elem);
+                    found = true;
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+
+                }
+                temp = temp.next_ind;
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Num not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+        }
+        public void Barrier(int data)
+        {
+            var time = new Stopwatch();
+            time.Start();
+            Node temp = head;
+            int count = 0;
+            while (temp.elem != data)
+            {
+                temp = temp.next_ind;
+                count++;
+            }
+            Console.WriteLine("Node found index: " + count);
+            time.Stop();
+            Console.WriteLine("Time spent: " + time.Elapsed);
+        }
+
+        static Node Midd(Node first, Node last)
+        {
+            Node mid = first;
+            Node temp = first.next_ind;
+
+            while (temp != last)
+            {
+                temp = temp.next_ind;
+                if (temp != last)
+                {
+                    temp = temp.next_ind;
+                    mid = mid.next_ind;
+                }
+            }
+            return mid;
+        }
+        static Node FindNode(Node head, int ind)
+        {
+            Node find = head;
+            int count = 0;
+            while (count != ind && find != null)
+            {
+                find = find.next_ind;
+                count++;
+            }
+            return find;
+        }
+        public int Size()
+        {
+            Node node = head;
+            int count = 0;
+            while (node != null)
+            {
+                count++;
+                node = node.next_ind;
+            }
+
+            return count;
+        }
+        public void LinkSortChoose()
+        {
+            Node first = head;
+            Node second = first.next_ind;
+
+            while (first != null)
+            {
+                while (second != null)
+                {
+                    if (first.elem > second.elem)
+                    {
+                        int temp = first.elem;
+                        first.elem = second.elem;
+                        second.elem = temp;
+                    }
+                    second = second.next_ind;
+                }
+                first = first.next_ind;
+                if (first.next_ind == null) { break; }
+                else { second = first.next_ind; }
+            }
+        }
+        public void Binary(int data)
+        {
+            var time = new Stopwatch();
+            time.Start();
+            Node first = head;
+            Node last = null;
+            bool found = false;
+
+            while (last != first)
+            {
+                Node mid = Midd(first, last);
+                if (mid.elem == data)
+                {
+                    Console.WriteLine("Node found : " + mid.elem);
+                    found = true;
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+                }
+                else if (mid.elem < data)
+                {
+                    first = mid.next_ind;
+                }
+                else
+                {
+                    last = mid;
+                }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Num not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+        }
+        public void BinaryGolden(int data, int size)
+        {
+            var time = new Stopwatch();
+            time.Start();
+            Node first = head;
+            Node last = null;
+            bool found = false;
+            int start = 0;
+            int end = size;
+
+            while (last != first)
+            {
+                int mid = (int)(start + (end - start) / 1.6);
+
+                Node node = FindNode(head, mid);
+
+
+                if (node.elem == data)
+                {
+                    Console.WriteLine("Node found : " + node.elem);
+                    found = true;
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+                }
+                else if (node.elem < data)
+                {
+                    first = node.next_ind;
+                    start = mid + 1;
+                }
+                else
+                {
+                    last = node;
+                    end = mid - 1;
+                }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Num not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+        }
+    }
+}
---
+++ Круць Михайло Ярославович/Lab_1_ASD/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab_1_ASD
+{
+     
+
+    class Program
+    {
+        static void PrintArr(int[] arr) { Console.WriteLine("[ " + string.Join(" ", arr) + " ]"); }
+        static int[] ChooseSort(int[] arr)
+        {
+            int temp = 0;
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                int low = arr[j];
+                for (int i = j; i < arr.Length; i++)
+                    if (arr[i] < low)
+                    {
+                        low = arr[i];
+                        temp = i;
+                    }
+
+                if (low != arr[j])
+                {
+                    arr[temp] = arr[j];
+                    arr[j] = low;
+                }
+            }
+            return arr;
+        }
+        static void LinearSearch(int[] arr, int num)
+        {
+            var time = new Stopwatch();
+            time.Start();
+            int j = 0;
+            bool found = false;
+
+            while (j < arr.Length)
+            {
+                if (arr[j] == num)
+                {
+                    found = true;
+                    Console.WriteLine("Found num index: " + j);
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+                }
+                j++;
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Element not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+        }
+        static void BarrSearch(int[] arr, int num)
+        {           
+            int[] new_arr = new int[arr.Length + 1];
+            Array.Copy(arr, new_arr, arr.Length);
+            new_arr[arr.Length] = num;
+            int i = 0;
+            var time = new Stopwatch();
+            time.Start();   
+            while (new_arr[i] != num) { i++; }
+            time.Stop();
+            Console.WriteLine("Found num index: " + i);                
+            Console.WriteLine("Time spent: " + time.Elapsed);      
+        }
+        static void BinarySearch(int[] arr, int num)
+        {
+            ChooseSort(arr);
+            int[] temp = new int[arr.Length];
+            Array.Copy(arr, temp, arr.Length);
+            var time = new Stopwatch();
+            time.Start();
+            int start = 0;
+            int end = temp.Length - 1;
+            bool found = false;
+            while (start <= end && found == false)
+            {
+                int mid = (start + end) / 2;
+                if (temp[mid] == num)
+                {
+                    found = true;
+                    Console.WriteLine("Found num " + num);
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+                }
+                else if (temp[mid] < num) { start = mid + 1; }
+                else { end = mid - 1; }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Number not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+           
+        }
+        static void BinaryGolden(int[] arr, int num) 
+        {
+            ChooseSort(arr);
+            int[] temp = new int[arr.Length];
+            Array.Copy(arr, temp, arr.Length);
+            
+            var time = new Stopwatch();
+            time.Start();
+            int start = 0;
+            int end = temp.Length - 1;
+            bool found = false;
+            while (start <= end && found == false)
+            {
+                int mid = (int)(start + (end - start) / 1.6);
+                if (temp[mid] == num)
+                {
+                    found = true;
+                    Console.WriteLine("Found num " + num);
+                    time.Stop();
+                    Console.WriteLine("Time spent: " + time.Elapsed);
+                    break;
+                }
+                else if (temp[mid] < num) { start = mid + 1; }
+                else { end = mid - 1; }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Number not found");
+                time.Stop();
+                Console.WriteLine("Time spent: " + time.Elapsed);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Start\n");
+                Console.WriteLine("Enter size:");
+                int size = Convert.ToInt32(Console.ReadLine());
+                Node linkedlist = new Node();
+                Node templist = new Node();
+                int[] arr = new int[size];
+                int a;
+                Random numRand = new Random();
+
+                Console.WriteLine("Input borders");
+                int bord1 = Convert.ToInt32(Console.ReadLine());
+                int bord2 = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < size; i++)
+                {
+                    a = numRand.Next(bord1, bord2);
+                    linkedlist.AddLast(a);
+                    templist.AddLast(a);
+                    arr[i] = a;
+                }               
+
+                PrintArr(arr);
+                Console.WriteLine("\n1.Linear search\n2.Search with a barrier\n3.Binary search\n4.Binary search with golden rule");
+                int ch = 1;
+                while (ch != 0)
+                {
+                    Console.WriteLine("Choose search algorythm:\n");                    
+                    ch = Convert.ToInt32(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case 0:
+                            Console.WriteLine("End");
+                            return;
+                        case 1:
+                            Console.WriteLine("Input num :");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Array:");
+                            LinearSearch(arr, num);
+                            Console.WriteLine("Linked list:");
+                            linkedlist.Linear(num);
+                            break;
+                        case 2:
+                            Console.WriteLine("Input num :");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Array:");
+                            BarrSearch(arr, num);
+                            Console.WriteLine("Linked list:");
+                            linkedlist.AddLast(num);
+                            linkedlist.Barrier(num);
+                            break;
+                        case 3:
+                            Console.WriteLine("Input num :");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Array:");
+                            BinarySearch(arr, num);
+                            Console.WriteLine("Linked list:");
+                            templist.LinkSortChoose();
+                            templist.Binary(num);
+                            break;
+                        case 4:
+                            Console.WriteLine("Input num :");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Array:");
+                            BinaryGolden(arr, num);
+                            Console.WriteLine("Linked list:");
+                            templist.LinkSortChoose();
+                            templist.BinaryGolden(num, linkedlist.Size());
+
+                            break;
+                        case 5:
+                            PrintArr(arr);
+                            break;
+                    }
+                }
+            }              
+        }
+    }
+}

c7d69059a7487a31b4a079c91d6d87da07cd38b9
Avruschenko Dmytro <68847010+TrippyFrenemy@users.noreply.github.com>
2022-04-19 13:40:59 +0300

    Лабораторна 1

---
+++ Аврущенко Дмитро Мансорович/Node.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD1_AvruschenkoTest
+{
+    internal class Node
+    {
+        public int Count { get; set; }
+        public int Data { get; set; }
+        public Node next_ind;
+        public Node head;
+        public Node()
+        {
+            Count = 0;
+            head = null;
+            next_ind = null;
+        }
+        public Node(int data)
+        {
+            Data = data;
+            next_ind = null;
+        }
+        public void AddLast(int data)
+        {
+            Count++;
+            if (head == null) { head = new Node(data); }
+            else
+            {
+
+                Node last = head;
+                while (last.next_ind != null) { last = last.next_ind; }
+                last.next_ind = new Node(data);
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                int counter = 0;
+                Node current = head;
+                while (current != null)
+                {
+                    if (counter == index)
+                    {
+                        return current.Data;
+                    }
+                    current = current.next_ind;
+                    counter++;
+                }
+                return -1;
+            }
+            set
+            {
+                int counter = 0;
+                Node current = head;
+                while (current != null)
+                {
+                    if (counter == index)
+                    {
+                        current.Data = value;
+                    }
+                    current = current.next_ind;
+                    counter++;
+                }
+            }
+        }
+        
+    }
+}
